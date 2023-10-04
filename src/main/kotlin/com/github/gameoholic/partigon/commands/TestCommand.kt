816768668438df597f6407594a7b70fa4d1beb67
Gameoholic <viner.atmic@gmail.com>
2023-10-04 12:19:27 +0300

    test matrix math

--- src/main/kotlin/com/github/gameoholic/partigon/commands/TestCommand.kt
+++ src/main/kotlin/com/github/gameoholic/partigon/commands/TestCommand.kt
@@ -1,29 +1,24 @@
 package com.github.gameoholic.partigon.commands
 
 
-import com.github.gameoholic.partigon.Utils
 import com.github.gameoholic.partigon.particle.PartigonParticle
 import com.github.gameoholic.partigon.particle.PartigonParticle.Companion.partigonParticle
 import com.github.gameoholic.partigon.particle.envelope.BasicEnvelope
 import com.github.gameoholic.partigon.particle.envelope.Envelope
-import com.github.gameoholic.partigon.particle.envelope.TrigonometricEnvelope
-import com.github.gameoholic.partigon.particle.envelope.wrapper.CircleEnvelopeWrapper
-import com.github.gameoholic.partigon.particle.envelope.wrapper.CircleEnvelopeWrapper.positionCircleEnvelopes
-import com.github.gameoholic.partigon.particle.loop.*
-
+import com.github.gameoholic.partigon.particle.loop.RepeatLoop
+import org.apache.commons.math3.geometry.Vector
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D
+import org.apache.commons.math3.linear.MatrixUtils
+import org.apache.commons.math3.linear.RealVector
 import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Particle
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
-import org.joml.Matrix2d
-import org.joml.Matrix2dc
-import org.joml.Matrix3d
-import org.joml.Vector2d
-import org.joml.Vector4f
 import kotlin.math.cos
 import kotlin.math.sin
+
 
 object TestCommand : CommandExecutor {
 
@@ -34,7 +29,27 @@ object TestCommand : CommandExecutor {
         val theta = 30.0
         val thetaRadians = Math.toRadians(theta)
 
-        org.bukkit.util.
+        val matrixData = arrayOf( //Rx(theta)
+            doubleArrayOf(1.0, 0.0, 0.0), //row 1
+            doubleArrayOf(0.0, cos(thetaRadians), -sin(thetaRadians)), //row 2
+            doubleArrayOf(0.0, sin(thetaRadians), cos(thetaRadians)) //row 3
+        )
+        val m = MatrixUtils.createRealMatrix(matrixData)
+
+
+        val matrixData2 = arrayOf( //Points
+            doubleArrayOf(1.0), //row 1
+            doubleArrayOf(1.0), //row 2
+            doubleArrayOf(1.0) //row 3
+        )
+        val m2 = MatrixUtils.createRealMatrix(matrixData2)
+
+        val newM = m.multiply(m2)
+
+        println(newM.data[0][0])
+        println(newM.data[1][0])
+        println(newM.data[2][0])
+
 
 
         partigonParticle(Location(Bukkit.getWorld("world"), 0.0, 100.0, 0.0), Particle.END_ROD) {
