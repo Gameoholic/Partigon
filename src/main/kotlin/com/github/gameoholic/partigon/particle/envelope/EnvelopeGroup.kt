dd7664fdb75799a2c406e5eb1dfef1800cf4c297
Gameoholic <viner.atmic@gmail.com>
2023-10-08 14:19:02 +0300

    feat: add support for <3 enveloeps in envelope group

--- src/main/kotlin/com/github/gameoholic/partigon/particle/envelope/EnvelopeGroup.kt
+++ src/main/kotlin/com/github/gameoholic/partigon/particle/envelope/EnvelopeGroup.kt
@@ -3,12 +3,11 @@ package com.github.gameoholic.partigon.particle.envelope
 import com.github.gameoholic.partigon.util.MatrixUtils
 
 class EnvelopeGroup(
-    val envelopeX: Envelope,
-    val envelopeY: Envelope,
-    val envelopeZ: Envelope,
+    val envelopeX: Envelope = StaticEnvelope(Envelope.PropertyType.POS_X, 0.0),
+    val envelopeY: Envelope = StaticEnvelope(Envelope.PropertyType.POS_Y, 0.0),
+    val envelopeZ: Envelope = StaticEnvelope(Envelope.PropertyType.POS_Z, 0.0),
     val rotationMatrixOptions: List<MatrixUtils.RotationMatrixOptions>
 ) {
-    //todo: support for <3 amount of envelopes.
 
     fun getEnvelopes(): List<Envelope> {
         envelopeX.envelopeGroup = this
