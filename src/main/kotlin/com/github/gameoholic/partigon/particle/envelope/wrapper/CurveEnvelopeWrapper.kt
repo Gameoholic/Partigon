f4bd296f042fe4d31539dd9ecee52ec46a1c4d76
Gameoholic <viner.atmic@gmail.com>
2023-10-08 14:04:26 +0300

    style: doc curve envelope

--- src/main/kotlin/com/github/gameoholic/partigon/particle/envelope/wrapper/CurveEnvelopeWrapper.kt
+++ src/main/kotlin/com/github/gameoholic/partigon/particle/envelope/wrapper/CurveEnvelopeWrapper.kt
@@ -10,7 +10,7 @@ object CurveEnvelopeWrapper {
 
     /**
      * Represents the interpolation type of the curve relative to a line connecting 2 points
-     * in 2D/3D space.
+     * in 3D space.
      * This enum provides a way to specify the direction/orientation of a curve relative
      * to a line from point 1 to point 2.
      *
@@ -20,81 +20,82 @@ object CurveEnvelopeWrapper {
      */
     enum class CurveOrientation {
         /**
-         * 2D Circle to the right of the line.
+         * 2D curve to the right of the line.
          */
         RIGHT,
 
         /**
-         * 2D Circle to the left of the line.
+         * 2D curve to the left of the line.
          */
         LEFT,
 
         /**
-         * 3D Circle from the right side of the line, below it.
+         * 3D curve from the right side of the line, below it.
          */
         RIGHT_DOWN,
 
         /**
-         * 3D Circle from the right side of the line, above it.
+         * 3D curve from the right side of the line, above it.
          */
         RIGHT_UP,
 
         /**
-         * 3D Circle from the left side of the line, below it.
+         * 3D curve from the left side of the line, below it.
          */
         LEFT_DOWN,
 
         /**
-         * 3D Circle from the left side of the line, above it.
+         * 3D curve from the left side of the line, above it.
          */
         LEFT_UP
     }
 
     /**
      * Represents the component (X,Y,Z) of a vector.
-     * This is used in circle envelopes with circle orientations to automatically
+     * This is used in curve envelopes with curve orientations to automatically
      * determine the trigonometric function to use.
      */
     enum class VectorComponent { X, Y, Z }
 
     /**
      * Trigonometric envelope wrapper that when applied on multiple properties,
-     * creates a circle between 2 points.
+     * creates a curve between 2 points.
      *
      * @param propertyType The property for the envelope to affect.
      * @param value1 The first value to interpolate.
      * @param value2 The second value to interpolate.
-     * @param circleDirection The orientation/direction of the circle.
+     * @param curveOrientation The orientation of the curve.
      * @param vectorComponent The vector component to be used for this circle property.
      * @param loop The loop to be used with the envelope.
-     * @param completion How much of the circle will be animated. If set to 1.0, an entire circle would be drawn. If set to 0.5, only half of it, etc.
+     * @param completion How much of the curve will be animated. If set to 1.0, the entire curve would be drawn. If set to 2.0, it'll draw a half-ellipse. If set to 4.0, it'll draw an ellipse.
      *
      * @throws IllegalArgumentException If an invalid combination of circle orientation & vector component was provided.
+     * @return The trigonometric envelope to be used on this property to create the curve.
      */
-    fun circleEnvelope(
+    fun curveEnvelope(
         propertyType: Envelope.PropertyType,
         value1: Any,
         value2: Any,
-        circleDirection: CurveOrientation,
+        curveOrientation: CurveOrientation,
         vectorComponent: VectorComponent,
         loop: Loop,
         completion: Double = 1.0,
     ): TrigonometricEnvelope {
         val trigFunc =
-            if ((circleDirection == CurveOrientation.LEFT || circleDirection == CurveOrientation.LEFT_UP || circleDirection == CurveOrientation.LEFT_DOWN) && vectorComponent == VectorComponent.X)
+            if ((curveOrientation == CurveOrientation.LEFT || curveOrientation == CurveOrientation.LEFT_UP || curveOrientation == CurveOrientation.LEFT_DOWN) && vectorComponent == VectorComponent.X)
                 TrigonometricEnvelope.TrigFunc.SIN
-            else if ((circleDirection == CurveOrientation.LEFT || circleDirection == CurveOrientation.LEFT_UP || circleDirection == CurveOrientation.LEFT_DOWN) && vectorComponent == VectorComponent.Z)
+            else if ((curveOrientation == CurveOrientation.LEFT || curveOrientation == CurveOrientation.LEFT_UP || curveOrientation == CurveOrientation.LEFT_DOWN) && vectorComponent == VectorComponent.Z)
                 TrigonometricEnvelope.TrigFunc.COS
-            else if ((circleDirection == CurveOrientation.RIGHT || circleDirection == CurveOrientation.RIGHT_UP || circleDirection == CurveOrientation.RIGHT_DOWN) && vectorComponent == VectorComponent.X)
+            else if ((curveOrientation == CurveOrientation.RIGHT || curveOrientation == CurveOrientation.RIGHT_UP || curveOrientation == CurveOrientation.RIGHT_DOWN) && vectorComponent == VectorComponent.X)
                 TrigonometricEnvelope.TrigFunc.COS
-            else if ((circleDirection == CurveOrientation.RIGHT || circleDirection == CurveOrientation.RIGHT_UP || circleDirection == CurveOrientation.RIGHT_DOWN) && vectorComponent == VectorComponent.Z)
+            else if ((curveOrientation == CurveOrientation.RIGHT || curveOrientation == CurveOrientation.RIGHT_UP || curveOrientation == CurveOrientation.RIGHT_DOWN) && vectorComponent == VectorComponent.Z)
                 TrigonometricEnvelope.TrigFunc.SIN
-            else if ((circleDirection == CurveOrientation.RIGHT_DOWN || circleDirection == CurveOrientation.LEFT_DOWN) && vectorComponent == VectorComponent.Y)
+            else if ((curveOrientation == CurveOrientation.RIGHT_DOWN || curveOrientation == CurveOrientation.LEFT_DOWN) && vectorComponent == VectorComponent.Y)
                 TrigonometricEnvelope.TrigFunc.SIN
-            else if ((circleDirection == CurveOrientation.RIGHT_UP || circleDirection == CurveOrientation.LEFT_UP) && vectorComponent == VectorComponent.Y)
+            else if ((curveOrientation == CurveOrientation.RIGHT_UP || curveOrientation == CurveOrientation.LEFT_UP) && vectorComponent == VectorComponent.Y)
                 TrigonometricEnvelope.TrigFunc.COS
             else
-                throw IllegalArgumentException("Invalid combination of circle orientation & vector component")
+                throw IllegalArgumentException("Invalid combination of curve orientation & vector component")
 
         return TrigonometricEnvelope(
             propertyType,
@@ -108,25 +109,26 @@ object CurveEnvelopeWrapper {
 
     /**
      * Trigonometric envelope wrapper that when applied on multiple properties,
-     * creates a circle between 2 points.
+     * creates a curve between 2 points.
      * This automatically determines the trigonometric function to use based
      * on the circle orientation and the property type.
-     * This method may only be used with vector property types (POS_X, POS_Y, POS_Z)
+     * This method may only be used with vector property types (POS_X, POS_Y, POS_Z), and is preferred
+     * if you are dealing with position envelopes.
      *
      * @param propertyType The property for the envelope to affect.
      * @param value1 The first value to interpolate.
      * @param value2 The second value to interpolate.
-     * @param circleDirection The orientation/direction of the circle.
+     * @param curveOrientation The orientation of the curve.
      * @param loop The loop to be used with the envelope.
-     * @param completion How much of the circle will be animated. If set to 1.0, an entire circle would be drawn. If set to 0.5, only half of it, etc.
+     * @param completion How much of the curve will be animated. If set to 1.0, the entire curve would be drawn. If set to 2.0, it'll draw a half-ellipse. If set to 4.0, it'll draw an ellipse.
      *
      * @throws IllegalArgumentException If the method doesn't support the property type provided.
      */
-    fun circleEnvelope(
+    fun curveEnvelope(
         propertyType: Envelope.PropertyType,
         value1: Any,
         value2: Any,
-        circleDirection: CurveOrientation,
+        curveOrientation: CurveOrientation,
         loop: Loop,
         completion: Double = 1.0,
     ): TrigonometricEnvelope {
@@ -138,11 +140,11 @@ object CurveEnvelopeWrapper {
                 else -> throw IllegalArgumentException()
             }
 
-        return circleEnvelope(
+        return curveEnvelope(
             propertyType,
             value1,
             value2,
-            circleDirection,
+            curveOrientation,
             vectorComponent,
             loop,
             completion,
@@ -150,28 +152,48 @@ object CurveEnvelopeWrapper {
     }
 
 
-    fun circleEnvelopeGroup(
-        position1: Utils.Pair<Double>,
-        position2: Utils.Pair<Double>,
-        circleDirection: CurveOrientation,
+    /**
+     * Envelope wrapper that creates a curve between 2 points
+     * in 3D space, with rotations.
+     *
+     * @param position1 The first value to interpolate.
+     * @param position2 The second value to interpolate.
+     * @param curveOrientation The orientation of the curve.
+     * @param rotationOptions The list of the rotations to apply to the circle.
+     * @param loop The loop to be used with the envelope.
+     * @param completion How much of the curve will be animated. If set to 1.0, the entire curve would be drawn. If set to 2.0, it'll draw a half-ellipse. If set to 4.0, it'll draw an ellipse.
+     *
+     * @return The envelope group used to create the curve.
+     */
+    fun curveEnvelopeGroup(
+        position1: Utils.Vector<Double>,
+        position2: Utils.Vector<Double>,
+        curveOrientation: CurveOrientation,
         rotationOptions: List<MatrixUtils.RotationMatrixOptions>,
         loop: Loop,
         completion: Double = 1.0,
     ): EnvelopeGroup = EnvelopeGroup(
-        circleEnvelope(
+        curveEnvelope(
             Envelope.PropertyType.POS_X,
             position1.x,
             position2.x,
-            circleDirection,
+            curveOrientation,
             loop,
             completion,
         ),
-        StaticEnvelope(Envelope.PropertyType.POS_Y, 0.0),
-        circleEnvelope(
+        curveEnvelope(
+            Envelope.PropertyType.POS_Y,
+            position1.y,
+            position2.y,
+            curveOrientation,
+            loop,
+            completion,
+        ),
+        curveEnvelope(
             Envelope.PropertyType.POS_Z,
             position1.z,
             position2.z,
-            circleDirection,
+            curveOrientation,
             loop,
             completion,
         ),
