734c9fd9358d68e13aa0a38563b9c4c0d67e7028
Gameoholic <viner.atmic@gmail.com>
2023-10-10 00:29:26 +0300

    docs: clarified continueloop docs

--- src/main/kotlin/com/github/gameoholic/partigon/particle/loop/ContinueLoop.kt
+++ src/main/kotlin/com/github/gameoholic/partigon/particle/loop/ContinueLoop.kt
@@ -2,12 +2,11 @@ package com.github.gameoholic.partigon.particle.loop
 
 /**
  * When loop reaches end, will continue running the animation, disregarding the loop.
- * This is the default loop used when no loop is provided.
+ * This loop does not affect the frame index.
  */
 class ContinueLoop(override val duration: Int) : Loop {
     override val envelopeDuration = duration
     override fun applyLoop(frameIndex: Int): Int {
         return frameIndex
     }
-
 }
