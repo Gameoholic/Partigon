019861a24803879383f7eb98db12bb7ccf3d9cbe
Gameoholic <viner.atmic@gmail.com>
2023-10-10 18:47:22 +0300

    feat: envelope group accepts offset as well as position

--- src/main/kotlin/com/github/gameoholic/partigon/particle/envelope/EnvelopeGroup.kt
+++ src/main/kotlin/com/github/gameoholic/partigon/particle/envelope/EnvelopeGroup.kt
@@ -4,24 +4,29 @@ import com.github.gameoholic.partigon.util.MatrixUtils
 import java.lang.RuntimeException
 
 /**
- * Used for grouping multiple position envelopes together.
+ * Used for grouping multiple position/offset envelopes together.
  * Sets the group of the envelopes provided to this one on init.
- * @param envelopeX The envelope used for the X_AXIS position.
- * @param envelopeY The envelope used for the Y_AXIS position.
- * @param envelopeZ The envelope used for the Z_AXIS position.
- * @param rotationMatrixOptions List of rotation options to be used on the positions.
+ * @param envelopeX The envelope used for the X component.
+ * @param envelopeY The envelope used for the Y component.
+ * @param envelopeZ The envelope used for the Z component.
+ * @param rotationMatrixOptions List of rotation options to be used on the components.
  *
  * @throws RuntimeException If one of the envelopes already has a group assigned.
+ * @throws IllegalArgumentException If one of the envelopes' properties is invalid.
  */
 class EnvelopeGroup(
-    val envelopeX: Envelope = ConstantEnvelope(Envelope.PropertyType.POS_X, 0.0),
-    val envelopeY: Envelope = ConstantEnvelope(Envelope.PropertyType.POS_Y, 0.0),
-    val envelopeZ: Envelope = ConstantEnvelope(Envelope.PropertyType.POS_Z, 0.0),
+    val envelopeX: Envelope, //todo: provide default values here, for offset AND position.
+    val envelopeY: Envelope,
+    val envelopeZ: Envelope,
     val rotationMatrixOptions: List<MatrixUtils.RotationOptions> = listOf()
 ) {
     init {
         if (envelopeX.envelopeGroup != null || envelopeY.envelopeGroup != null || envelopeZ.envelopeGroup != null)
             throw RuntimeException("Envelopes may only have one envelope group assigned to them.")
+        if (envelopeX.propertyType != Envelope.PropertyType.POS_X && envelopeX.propertyType != Envelope.PropertyType.OFFSET_X &&
+            envelopeY.propertyType != Envelope.PropertyType.POS_Y && envelopeY.propertyType != Envelope.PropertyType.OFFSET_Y &&
+            envelopeZ.propertyType != Envelope.PropertyType.POS_Z && envelopeZ.propertyType != Envelope.PropertyType.OFFSET_Z)
+            throw IllegalArgumentException("One of the envelopes' properties is invalid.")
         envelopeX.envelopeGroup = this
         envelopeY.envelopeGroup = this
         envelopeZ.envelopeGroup = this
