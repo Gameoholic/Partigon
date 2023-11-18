61e721640a6395541fc02dc08df2311716d22bbe
Game <viner.atmic@gmail.com>
2023-11-18 16:42:36 +0200

    add todos

--- src/main/kotlin/xyz/gameoholic/partigon/particle/MultiParticle.kt
+++ src/main/kotlin/xyz/gameoholic/partigon/particle/MultiParticle.kt
@@ -2,6 +2,8 @@ package xyz.gameoholic.partigon.particle
 
 class MultiParticle(private val particles: List<SingularParticle>): PartigonParticle {
 
+    //todo: doc different partigon classes
+    //todo: add inline funs here
     override fun start() {
         particles.forEach {
             it.start()
